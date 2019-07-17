24f60f3cfd64121379a9c6360b6ef5f583cc4e17
yangwensen <fjzpyws@qq.com>
2019-07-17 22:47:06 +0800

    [spu]ciu98m25_read() succeed now,awesome!

--- User/ciu98m25.c
+++ User/ciu98m25.c
@@ -6,6 +6,7 @@
 #define CIU98M25_SPI_INIT()         SPI_Init()
 #define CIU98M25_SPI_TRANSFER(a)    SPI_TransferByte(a)
 #define CIU98M25_SPI_SEND(a,b)    	spi_burst_send(a,b)
+#define CIU98M25_SPI_RECV(a,b)    	spi_burst_recv(a,b)
 
 #define CIU98M25_CS_PORT			PA
 #define CIU98M25_CS_PIN				3
@@ -94,6 +95,15 @@ static void spi_burst_send(uint8 *buff, uint8 len)
 	}
 }
 //************************************************************************************************************
+//by yangwensen@20190717
+static void spi_burst_recv(uint8 *buff, uint8 len)
+{
+ 	while(len--)
+	{
+	  	*buff++ = CIU98M25_SPI_TRANSFER(0xff);
+	}
+}
+//************************************************************************************************************
 static sint8 ciu98m25_write(uint8 *buff, uint16 len)
 {
 	uint8 check_code;
@@ -166,17 +176,15 @@ SEND_LAST_PACK:
 }
 //************************************************************************************************************
 //by yangwensen@20190715
-static sint16 ciu98m25_read(uint8 *buff, uint16 buff_len)
+static sint16 ciu98m25_read(uint8 *rx_buff, uint16 buff_len)
 {
   	uint8 sync[2];
 	uint16 expect_len;
-	uint8 packs;
-	uint8 last_pack_bytes;
 	uint8 check_code;
-	uint8 i,j;
-	uint16 a;
+	uint8 *buff;
 	
-	if(!buff)return -1;
+	if(!rx_buff)return -1;
+	buff = rx_buff;
 	
 	//receive sync
 	CIU98M25_CS_ASSERT();
@@ -190,7 +198,6 @@ static sint16 ciu98m25_read(uint8 *buff, uint16 buff_len)
 		return -2;
 	}
 	
-	PrintHexArray(sync, 2);
 	if(sync[0]!=0xaa)return -3;
 	if(sync[1]!=0x55)return -4;
 //==============================================================================
@@ -198,30 +205,36 @@ static sint16 ciu98m25_read(uint8 *buff, uint16 buff_len)
 	CIU98M25_CS_ASSERT();
 	sync[0] = CIU98M25_SPI_TRANSFER(0xff);
 	sync[1] = CIU98M25_SPI_TRANSFER(0xff);
+
+	expect_len = (((uint16)sync[0])<<8) | sync[1];
+	if(expect_len > buff_len)return -5;
+//==============================================================================
+	if(expect_len<=13)		//first pack = last pack
+	{
+		goto RECV_LAST_PACK;
+	}
+	
+	CIU98M25_SPI_RECV(buff, 14);
 	CIU98M25_CS_DEASSERT();
+	buff += 14;
+	expect_len -= 14;
 	
 	OSSemCreate(&sem_ciu98m25_ready, 0);
 	if( OSSemPend(&sem_ciu98m25_ready, CIU98M25_WRITE_TIMEOUT)!=OS_NO_ERR )
 	{
-		return -5;
+		return -6;
 	}
-	
-	expect_len = (((uint16)sync[0])<<8) | sync[1];
-	if(expect_len > buff_len)return -6;
 //==============================================================================
-	packs = expect_len / 16;
-	last_pack_bytes = expect_len % 16;
-//==============================================================================
-	//receive 16-byte data	
-	for(i=0; i<packs; i++)
+	//middle full packet(16 bytes)
+	while(expect_len>=16)
 	{
 		CIU98M25_CS_ASSERT();
-	  	for(j=0; j<16; j++)
-		{
-			*buff++ = CIU98M25_SPI_TRANSFER(0xff);
-		}
+		CIU98M25_SPI_RECV(buff, 16);
 		CIU98M25_CS_DEASSERT();
-		
+		buff += 16;
+		expect_len -= 16;
+	  	
+		OSSemCreate(&sem_ciu98m25_ready, 0);
 		if( OSSemPend(&sem_ciu98m25_ready, CIU98M25_WRITE_TIMEOUT)!=OS_NO_ERR )
 		{
 			return -7;
@@ -229,51 +242,21 @@ static sint16 ciu98m25_read(uint8 *buff, uint16 buff_len)
 	}
 //==============================================================================
 	//receive last pack data(less than 16 bytes)
-	if(last_pack_bytes)
-	{
-		CIU98M25_CS_ASSERT();
-		for(j=0; j<last_pack_bytes; j++)
-		{
-			*buff++ = CIU98M25_SPI_TRANSFER(0xff);
-		}
-		CIU98M25_CS_DEASSERT();
-		
-		if( OSSemPend(&sem_ciu98m25_ready, CIU98M25_WRITE_TIMEOUT)!=OS_NO_ERR )
-		{
-			return -8;
-		}
-	}
-//==============================================================================	
-	//receive check_code
+RECV_LAST_PACK:
 	CIU98M25_CS_ASSERT();
+	CIU98M25_SPI_RECV(buff, expect_len);
 	check_code = CIU98M25_SPI_TRANSFER(0xff);
 	CIU98M25_CS_DEASSERT();
-	
+  
 	OSSemCreate(&sem_ciu98m25_ready, 0);
 	if( OSSemPend(&sem_ciu98m25_ready, CIU98M25_WRITE_TIMEOUT)!=OS_NO_ERR )
 	{
-		return -9;
+		return -8;
 	}
 //==============================================================================
 	//verify received data
-	packs = 0;
-	for(a=0; a<expect_len; a++)
-	{
-	  	packs ^= buff[a];
-	}
-	if(~packs != check_code)return -10;
-//==============================================================================
-	//send confirmation
-	CIU98M25_CS_ASSERT();
-	CIU98M25_SPI_TRANSFER(0xaa);
-	CIU98M25_SPI_TRANSFER(0xaa);
-	CIU98M25_CS_DEASSERT();
-	
-	OSSemCreate(&sem_ciu98m25_ready, 0);
-	if( OSSemPend(&sem_ciu98m25_ready, CIU98M25_WRITE_TIMEOUT)!=OS_NO_ERR )
-	{
-		return -11;
-	}
+	expect_len = (((uint16)sync[0])<<8) | sync[1];
+	if(check_code != calc_check_code(rx_buff,expect_len) )return -9;
 //==============================================================================
   	return expect_len;
 }
@@ -290,7 +273,7 @@ extern sint16 ciu98m25_transfer(uint8 *tbuff, uint16 tlen, uint8 *rbuff, uint16 
 	IO_MODE_PP(CIU98M25_SLEEP_PORT, CIU98M25_SLEEP_PIN);
 	IO_CLR(CIU98M25_SLEEP_PORT, CIU98M25_SLEEP_PIN);
 	
-	OSTimeDly(10);
+	OSTimeDly(1);
 //==============================================================================
 	result = ciu98m25_write(tbuff, tlen);
 	if(result < 0)
@@ -303,6 +286,7 @@ extern sint16 ciu98m25_transfer(uint8 *tbuff, uint16 tlen, uint8 *rbuff, uint16 
 	read_len = ciu98m25_read(rbuff, rlen);
 	if(read_len < 0)
 	{
+		CIU98M25_CS_DEASSERT();
 		IO_SET(CIU98M25_SLEEP_PORT, CIU98M25_SLEEP_PIN);
 		PRINT_ERROR_CODE("[Y]ciu98m25_transfer_rd", read_len);
 		return -2;
@@ -330,15 +314,15 @@ static void print_error_code(uint8 *str, uint8 strlen, sint8 err)
 }
 //************************************************************************************************************
 //by yangwensen@20190715
+uint8 spu_rx_buff[255];
 extern void ciu98m25_test(void)
 {
 	uint8 t[] = {0x00, 0xa4, 0x04, 0x00, 0x00};
-	uint8 r[4];
 	sint16 len;
 	
-	len = ciu98m25_transfer(t, sizeof(t), r, sizeof(r));
+	len = ciu98m25_transfer(t, sizeof(t), spu_rx_buff, sizeof(spu_rx_buff));
 	if(len<0)PRINT_ERROR_CODE("[Y]ciu98m25_transfer failed", len);
-	else PrintHexArray(r, len);
+	else PrintHexArray(spu_rx_buff, len);
 	
 }
 //************************************************************************************************************
